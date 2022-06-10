b3ecc1645cf47d3c390463ea425be1302878ec67
August Ollé Corbella <100363185+Atorremedia@users.noreply.github.com>
2022-06-10 15:39:30 +0200

    Working basket

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
-import Basket from './components/basket';
+import Basket from './components/Basket';
+import data from './data';
+import { useState } from 'react';
 
 function App() {
+  const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
+
+  const onAdd =(product) =>{
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
   return (
     <div className="App">
-      <div>
-        <Header></Header>
-        <Main></Main>
-        <Basket></Basket>
+      <Header></Header>
+      <div className="row">
+        <Main products={products} onAdd={onAdd} onRemove={onRemove}></Main>
+        <Basket cartItems={cartItems} onAdd={onAdd} onRemove={onRemove}></Basket>
      </div>
     </div>
   );
