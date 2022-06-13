da31383e657e10965a474dac9d07071b66995193
August Ollé Corbella <100363185+Atorremedia@users.noreply.github.com>
2022-06-13 11:20:03 +0200

    basket and summary

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 export default function Header(props) {
     return (
-    <header className="row block center">
+    <header className="row block center header">
+            <div className="logo">WS</div>
         <div>
             <a href="#/">
                 <h1>Your webstore</h1>
             </a>
         </div>
         <div>
-            <a href="#/cart">Cart </a> <a href="#/signin"> SignIn</a>
+            <a href="#/signin"> SignIn</a>
 
         </div>
     </header>
--- src/components/basket.jsx
+++ src/components/basket.jsx
@@ -2,22 +2,48 @@ import React from 'react';
 
 export default function Basket(props) {
     const {cartItems, onAdd, onRemove} = props;
+    const itemsPrice = cartItems.reduce((acc, cur) => acc + cur.price * cur.qty, 0);
+    const taxPrice = itemsPrice * 0.21;
+    const shippingPrice = itemsPrice > 50 ? 0 : 28;
+    const totalPrice = itemsPrice + taxPrice + shippingPrice;
     return <aside className="block col-1">
         <h2>Cart Items</h2>
         <div>
             {cartItems.length === 0 && <div>Cart is Empty</div>}
         </div>
         {cartItems.map((item) => (
-                    <div key={item.id} className="row">
+                    <div key={item.id} className="row basketproduct">
                         <div className="col-2">{item.name}</div>
-                        <div className="col-2">
-                            <button onClick={()=>onAdd(item)} className="add">+</button>
-                            <button onClick={()=>onRemove(item)} className="remove">-</button>
+                        <div className="col-2 addremove">
+                        <button onClick={()=>onRemove(item)} className="remove">-</button>
+                        <button onClick={()=>onAdd(item)} className="add">+</button>
                         </div>
                         <div className="col-2 text-right">
-                            {item.qty} X ${item.price.toFixed(2)}
+                            <span className="quantity">{item.qty} X</span><span className="price"> ${item.price.toFixed(2)}</span>
                         </div>
                     </div>
                 ))}
+                {cartItems.length !== 0 &&(
+                    <>
+                        <hr></hr>
+                        <div className="row">
+                            <div className="col-2">Items Price</div>
+                            <div className="col-1 text-right">${itemsPrice.toFixed(2)}</div>
+                        </div>
+                        <div className="row">
+                            <div className="col-2">Tax Price (21%)</div>
+                            <div className="col-1 text-right">${taxPrice.toFixed(2)}</div>
+                        </div>
+                        <div className="row">
+                            <div className="col-2" title="Over $50 no shipping is charged">Shipping Price</div>
+                            <div className="col-1 text-right" title="Over $50 no shipping is charged">${shippingPrice.toFixed(2)}</div>
+                        </div>
+                        <div className="row">
+                            <div className="col-2"><strong>Total Price</strong></div>
+                            <div className="col-1 text-right"><strong>${totalPrice.toFixed(2)}</strong></div>
+                        </div>
+                        <button className="checkout" onClick={()=>alert('This would bring you to a data form and a payment page')}>Check out</button>
+                    </>
+                )}
     </aside>;
 }
